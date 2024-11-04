a62675eaa8a78c7c9c6f79bf2baf120d539af45c
Purushottam Shirodkar <purushottam.shirodkar@blazeclan.com>
2024-11-05 12:02:09 +1300

    adding tf file

--- terraform_code_puru/V1-EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "ap-southeast-2"
-}
-
-resource "aws_instance" "demo" {
-  ami           = "ami-0f71013b2c8bd2c29"
-  instance_type = "t2.micro"
-  key_name       = "DevOps-Project_Key"
-}
